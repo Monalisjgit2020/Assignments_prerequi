1895261384aa56d96e036b39ce336c5e3827e9aa
Monalisjgit2020 <monalicp@gmail.com>
2020-02-11 22:47:43 -0600

    I have added Test case 1

--- GitSample2/src/JavaSample.java
+++ GitSample2/src/JavaSample.java
@@ -1,10 +1,15 @@
 
 public class JavaSample {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		//code written by Monali
+		//code written by Monali Jawalkar
 
+	}
+	public void TestCase1()
+	{
+		// code of test case will be written here
 	}
 
 }
